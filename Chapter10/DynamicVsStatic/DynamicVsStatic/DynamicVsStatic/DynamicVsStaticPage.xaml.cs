c27a699c91df36ff0bf3e087056bcf92c974f6f1
Charles Petzold <charles.petzold@xamarin.com>
2015-02-20 10:52:16 -0500

    Chapter 10 initial publication

---
+++ Chapter10/DynamicVsStatic/DynamicVsStatic/DynamicVsStatic/DynamicVsStaticPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DynamicVsStatic
+{
+    public partial class DynamicVsStaticPage : ContentPage
+    {
+        public DynamicVsStaticPage()
+        {
+            InitializeComponent();
+
+            Device.StartTimer(TimeSpan.FromSeconds(1),
+                () =>
+                {
+                    Resources["currentDateTime"] = DateTime.Now.ToString();
+                    return true;
+                });
+        }
+    }
+}
+
---
+++ Chapter10/SharedStatics/SharedStatics/SharedStatics.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SharedStatics.WinPhone.Resources;
+
+namespace SharedStatics.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
