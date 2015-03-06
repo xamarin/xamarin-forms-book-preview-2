d64df45e3a9e5ce9b62ea41ba602f734a6dfcff1
Charles Petzold <charles.petzold@xamarin.com>
2015-03-06 12:06:15 -0500

    Chapter 12 initial publication

---
+++ Chapter12/BasicStyle/BasicStyle/BasicStyle.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using BasicStyle.WinPhone.Resources;
+
+namespace BasicStyle.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
