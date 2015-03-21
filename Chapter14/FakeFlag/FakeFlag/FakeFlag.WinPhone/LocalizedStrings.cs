6318f3ab335e7d154a1aaa34372ea13a216656ef
Charles Petzold <charles.petzold@xamarin.com>
2015-03-21 12:11:29 -0400

    Chapter 14 FakeFlag Program

---
+++ Chapter14/FakeFlag/FakeFlag/FakeFlag.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using FakeFlag.WinPhone.Resources;
+
+namespace FakeFlag.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ Chapter14/FakeFlag/FakeFlag/FakeFlag/App.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FakeFlag
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new FakeFlagPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
