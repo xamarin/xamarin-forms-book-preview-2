e54f6ef0b8e6bad7db8e17e6d09fc767b38611cc
Charles Petzold <charles.petzold@xamarin.com>
2015-03-22 13:39:06 -0400

    Chapter 14 ProportionalCoordinateCalc

---
+++ Chapter14/ProportionalCoordinateCalc/ProportionalCoordinateCalc/ProportionalCoordinateCalc.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ProportionalCoordinateCalc.WinPhone.Resources;
+
+namespace ProportionalCoordinateCalc.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
