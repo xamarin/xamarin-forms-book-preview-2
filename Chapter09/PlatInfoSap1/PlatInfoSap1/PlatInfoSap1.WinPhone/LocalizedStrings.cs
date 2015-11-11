b750005057d1558b82c8750cff9fbfe1bd87ef8a
Charles Petzold <charles.petzold@xamarin.com>
2015-11-11 15:52:14 -0500

    Rename DisplayPlatformInfoSap* program
    Avoids problem with overlong file paths in SAP solutions.

---
+++ Chapter09/PlatInfoSap1/PlatInfoSap1/PlatInfoSap1.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PlatInfoSap1.WinPhone.Resources;
+
+namespace PlatInfoSap1.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
