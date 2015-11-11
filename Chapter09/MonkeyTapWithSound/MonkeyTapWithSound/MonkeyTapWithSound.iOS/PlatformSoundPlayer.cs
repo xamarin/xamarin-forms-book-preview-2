619dec564bf05204b0c928ef342b6d4a948a0757
Charles Petzold <charles.petzold@xamarin.com>
2015-11-11 15:04:11 -0500

    Fix iOS MonkeyTapWithSound

--- Chapter09/MonkeyTapWithSound/MonkeyTapWithSound/MonkeyTapWithSound.iOS/PlatformSoundPlayer.cs
+++ Chapter09/MonkeyTapWithSound/MonkeyTapWithSound/MonkeyTapWithSound.iOS/PlatformSoundPlayer.cs
@@ -16,7 +16,7 @@ namespace MonkeyTapWithSound.iOS
 
         public void PlaySound(int samplingRate, byte[] pcmData)
         {
-            int numSamples = pcmData.Length / (bitsPerSample / 8); 
+            int numSamples = pcmData.Length / (bitsPerSample / 8);
 
             MemoryStream memoryStream = new MemoryStream();
             BinaryWriter writer = new BinaryWriter(memoryStream, Encoding.ASCII);
@@ -28,7 +28,7 @@ namespace MonkeyTapWithSound.iOS
             writer.Write(new char[] { 'f', 'm', 't', ' ' });                // format chunk
             writer.Write(16);                                               // PCM chunk size
             writer.Write((short)1);                                         // PCM format flag
-            writer.Write((short)numChannels);  
+            writer.Write((short)numChannels);
             writer.Write(samplingRate);
             writer.Write(samplingRate * numChannels * bitsPerSample / 8);   // byte rate
             writer.Write((short)(numChannels * bitsPerSample / 8));         // block align
